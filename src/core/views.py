2632d13087a34a97b33ae1b45b1cc6143db1235d
heffis <heffergabriel9@gmail.com>
2020-06-24 01:06:17 -0300

    context in render

--- src/core/views.py
+++ src/core/views.py
@@ -5,7 +5,8 @@ from .models import Request
 def index(request):
     return render(request, 'index.html')
 
+
 def info(request):
     requests_base = Request.object.all()
     context = {request.nome: request.urlBase for request in requests_base}
-    return render(request, 'info.html')
+    return render(request, 'info.html', context)
