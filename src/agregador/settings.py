082e03dd31ab52ed7379be3dfcabc5f8db2e4b40
heffis <heffergabriel9@gmail.com>
2020-06-24 00:42:41 -0300

    settings and url django path

--- src/agregador/settings.py
+++ src/agregador/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uykbsk5#in+hdcc-2aki-o*a2373sg9s69+sfi#k_u5p&^6s6o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'statictfiles')
